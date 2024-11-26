67ade69783041bdc407579de70a8af126d9a53eb
ezcanillo <168624885+ezcanillo@users.noreply.github.com>
2024-11-27 05:38:08 +0800

    Create AACTNUM4 Form3.Desig.cs

---
+++ AACTNUM4 Form3.Desig.cs
@@ -0,0 +1,154 @@
+namespace AACTNUM4
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            programsToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            aboutTheProgrammersToolStripMenuItem = new ToolStripMenuItem();
+            calculatorToolStripMenuItem = new ToolStripMenuItem();
+            picturesToolStripMenuItem = new ToolStripMenuItem();
+            treeviewsToolStripMenuItem = new ToolStripMenuItem();
+            listviewToolStripMenuItem = new ToolStripMenuItem();
+            problemSolvingMindsetToolStripMenuItem = new ToolStripMenuItem();
+            attentionToDetailToolStripMenuItem = new ToolStripMenuItem();
+            patienceToolStripMenuItem = new ToolStripMenuItem();
+            logicalThinkingToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { programsToolStripMenuItem, aboutToolStripMenuItem, aboutTheProgrammersToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(515, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // programsToolStripMenuItem
+            // 
+            programsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { calculatorToolStripMenuItem, picturesToolStripMenuItem, treeviewsToolStripMenuItem, listviewToolStripMenuItem });
+            programsToolStripMenuItem.Name = "programsToolStripMenuItem";
+            programsToolStripMenuItem.Size = new Size(86, 24);
+            programsToolStripMenuItem.Text = "Programs";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(27, 24);
+            aboutToolStripMenuItem.Text = " ";
+            // 
+            // aboutTheProgrammersToolStripMenuItem
+            // 
+            aboutTheProgrammersToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { problemSolvingMindsetToolStripMenuItem, attentionToDetailToolStripMenuItem, patienceToolStripMenuItem, logicalThinkingToolStripMenuItem });
+            aboutTheProgrammersToolStripMenuItem.Name = "aboutTheProgrammersToolStripMenuItem";
+            aboutTheProgrammersToolStripMenuItem.Size = new Size(185, 24);
+            aboutTheProgrammersToolStripMenuItem.Text = "About The Programmers";
+            // 
+            // calculatorToolStripMenuItem
+            // 
+            calculatorToolStripMenuItem.Name = "calculatorToolStripMenuItem";
+            calculatorToolStripMenuItem.Size = new Size(224, 26);
+            calculatorToolStripMenuItem.Text = "calculator";
+            // 
+            // picturesToolStripMenuItem
+            // 
+            picturesToolStripMenuItem.Name = "picturesToolStripMenuItem";
+            picturesToolStripMenuItem.Size = new Size(224, 26);
+            picturesToolStripMenuItem.Text = "pictures";
+            // 
+            // treeviewsToolStripMenuItem
+            // 
+            treeviewsToolStripMenuItem.Name = "treeviewsToolStripMenuItem";
+            treeviewsToolStripMenuItem.Size = new Size(224, 26);
+            treeviewsToolStripMenuItem.Text = "Treeview";
+            // 
+            // listviewToolStripMenuItem
+            // 
+            listviewToolStripMenuItem.Name = "listviewToolStripMenuItem";
+            listviewToolStripMenuItem.Size = new Size(224, 26);
+            listviewToolStripMenuItem.Text = "Listview";
+            // 
+            // problemSolvingMindsetToolStripMenuItem
+            // 
+            problemSolvingMindsetToolStripMenuItem.Name = "problemSolvingMindsetToolStripMenuItem";
+            problemSolvingMindsetToolStripMenuItem.Size = new Size(260, 26);
+            problemSolvingMindsetToolStripMenuItem.Text = "Problem-Solving Mindset";
+            // 
+            // attentionToDetailToolStripMenuItem
+            // 
+            attentionToDetailToolStripMenuItem.Name = "attentionToDetailToolStripMenuItem";
+            attentionToDetailToolStripMenuItem.Size = new Size(260, 26);
+            attentionToDetailToolStripMenuItem.Text = "Attention to Detail";
+            // 
+            // patienceToolStripMenuItem
+            // 
+            patienceToolStripMenuItem.Name = "patienceToolStripMenuItem";
+            patienceToolStripMenuItem.Size = new Size(260, 26);
+            patienceToolStripMenuItem.Text = "Patience";
+            // 
+            // logicalThinkingToolStripMenuItem
+            // 
+            logicalThinkingToolStripMenuItem.Name = "logicalThinkingToolStripMenuItem";
+            logicalThinkingToolStripMenuItem.Size = new Size(260, 26);
+            logicalThinkingToolStripMenuItem.Text = "Logical Thinking";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(515, 215);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form3";
+            Text = "Form3";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem programsToolStripMenuItem;
+        private ToolStripMenuItem calculatorToolStripMenuItem;
+        private ToolStripMenuItem picturesToolStripMenuItem;
+        private ToolStripMenuItem treeviewsToolStripMenuItem;
+        private ToolStripMenuItem listviewToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem aboutTheProgrammersToolStripMenuItem;
+        private ToolStripMenuItem problemSolvingMindsetToolStripMenuItem;
+        private ToolStripMenuItem attentionToDetailToolStripMenuItem;
+        private ToolStripMenuItem patienceToolStripMenuItem;
+        private ToolStripMenuItem logicalThinkingToolStripMenuItem;
+    }
+}
